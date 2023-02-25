50467df2cf1d1f1641f0e23f04eb8cd44233f7f5
Xai Hang <xaihang12@gmail.com>
2023-02-25 11:44:48 -0600

    commentform changed text field width to 40ch

--- src/components/Form/CommentForm.jsx
+++ src/components/Form/CommentForm.jsx
@@ -29,7 +29,7 @@ export default function Form({ name, question, url }) {
       <Box
         component="form"
         sx={{
-          '& > :not(style)': { m: 1, width: '25ch' },
+          '& > :not(style)': { m: 1, width: '40ch' },
         }}
         noValidate
         autoComplete="off"
